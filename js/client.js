d4ff36c0c77174cec812fced3e65967271d07f14
Martin John Madsen <madsen.mj@gmail.com>
2018-02-04 08:27:45 -0500

    adjusted swipe

--- js/client.js
+++ js/client.js
@@ -100,8 +100,8 @@ var beginGame = function(){
     startY,
     distX,
     distY,
-    threshold = 150, //required min distance traveled to be considered swipe
-    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+    threshold = 30, //required min distance traveled to be considered swipe
+    restraint = 10, // maximum distance allowed at the same time in perpendicular direction
     allowedTime = 300, // maximum time allowed to travel that distance
     elapsedTime,
     startTime,
